624bf96f1f5b68e57c8f40ca3f768eb06a20735b
kastorcode <ramalholiveira13@gmail.com>
2024-09-18 17:39:43 -0300

    Add 4 stages: 2, 3, 4, 5

--- client/src/game/util/bgSound.ts
+++ client/src/game/util/bgSound.ts
@@ -4,7 +4,11 @@
 */
 const SOUNDS:{[key:number]:number[]} = {
   0: [0,1],
-  1: [0,1]
+  1: [0,1],
+  2: [0,1],
+  3: [0,1],
+  4: [0,1],
+  5: [0,1]
 }
 
 export function getBgSound (name:number) {
